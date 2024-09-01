1271b79a7f1455bdf14362af449f8374271662c5
Adnan <adbak.dev@gmail.com>
2024-09-02 01:09:14 +0200

    test

--- components/Chatbot.tsx
+++ components/Chatbot.tsx
@@ -1,30 +1,26 @@
-// components/Chatbot/Chatbot.tsx
 "use client";
 
 import { useFormState } from "react-dom";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { getChatbotResponse } from "@/actions/chatbot";
 import SubmitButton from "./SubmitButton";
 import ChatbotResponse from "./ChatbotResponse";
 
 export default function Chatbot() {
-  const [message, setMessage] = useState("");
   const [state, formAction] = useFormState(
-    async (_: any, formData: FormData) => {
+    async (prevState: any, formData: FormData) => {
       const message = formData.get("message") as string;
-      return getChatbotResponse(message);
+      const responseObj = await getChatbotResponse(message);
+      return { ...prevState, response: responseObj.response };
     },
     { response: "" }
   );
-
   return (
     <div className="max-w-md mx-auto mt-10 text-black">
       <form action={formAction} className="mb-4">
         <input
           type="text"
           name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
           placeholder="Ask about movies..."
           className="w-full p-2 border rounded"
         />
