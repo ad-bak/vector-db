7debf3451134699617c04c50c19a08adf37158c0
Adnan <adbak.dev@gmail.com>
2024-09-01 19:41:27 +0200

    config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,28 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (
-  !process.env.DB_HOST ||
-  !process.env.DB_PORT ||
-  !process.env.DB_USERNAME ||
-  !process.env.DB_PASSWORD ||
-  !process.env.DB_NAME
-) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DB_HOST is not defined");
 }
 
 export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./migrations",
   dialect: "postgresql",
-  schema: "./drizzle/schema.ts",
-  out: "./drizzle",
   dbCredentials: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: false,
+    url: process.env.DATABASE_URL,
   },
-  verbose: true,
-  strict: true,
 });
